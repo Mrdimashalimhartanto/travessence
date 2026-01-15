f71d1d37a53440d0359fdfb706b4cc5d383a8c2c
Dimas Halim Hartanto <dimashalimgit@gmail.com>
2026-01-15 09:11:16 +0700

    chore: remove build artifacts and update gitignore

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/dimashalimhartanto/Development/PROJECT/travessence/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/dimashalimhartanto/Development/PROJECT/travessence/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_foundation.build/Objects-normal/arm64/path_provider_foundation_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/dimashalimhartanto/Development/PROJECT/travessence/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_foundation.build/module.modulemap \
-  /Users/dimashalimhartanto/Development/PROJECT/travessence/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_foundation.build/DerivedSources/path_provider_foundation_vers.c
